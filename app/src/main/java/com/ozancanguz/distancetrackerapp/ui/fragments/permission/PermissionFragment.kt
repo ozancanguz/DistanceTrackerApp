a231f766d4007c01cb7abd41087ac6d702d321fb
Ozan <ozancangu24@gmail.com>
2023-02-09 20:31:13 +0300

    -view binding added

--- app/src/main/java/com/ozancanguz/distancetrackerapp/ui/fragments/permission/PermissionFragment.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/ui/fragments/permission/PermissionFragment.kt
@@ -5,18 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ozancanguz.distancetrackerapp.R
+import com.ozancanguz.distancetrackerapp.databinding.FragmentPermissionBinding
 
 
 class PermissionFragment : Fragment() {
+    private var _binding: FragmentPermissionBinding? = null
+
+    private val binding get() = _binding!!
+
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_permission, container, false)
+         _binding = FragmentPermissionBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
     }
 
 
--- app/src/main/java/com/ozancanguz/distancetrackerapp/ui/fragments/result/ResultFragment.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/ui/fragments/result/ResultFragment.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ozancanguz.distancetrackerapp.R
+import com.ozancanguz.distancetrackerapp.databinding.FragmentResultBinding
 
 
-class ResultFragment : Fragment() { // TODO: Rename and change types of parameters
+class ResultFragment : Fragment() {
+    private var _binding: FragmentResultBinding? = null
+
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+
+         _binding = FragmentResultBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
 
