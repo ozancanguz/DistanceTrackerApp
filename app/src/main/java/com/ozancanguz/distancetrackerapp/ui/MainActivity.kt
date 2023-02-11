010a7644e9dfea2478850d719df85348e5909195
Ozan <ozancangu24@gmail.com>
2023-02-11 16:26:29 +0300

    -google maps api added

--- app/src/main/java/com/ozancanguz/distancetrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/ui/MainActivity.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.ozancanguz.distancetrackerapp.R
+import com.ozancanguz.distancetrackerapp.utils.Permissions.hasLocationPermission
 
 class MainActivity : AppCompatActivity() {
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
