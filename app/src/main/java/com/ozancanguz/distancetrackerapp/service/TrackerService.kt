68408c7dbe3537c15edd50adfd8ec5d44d169e59
Ozan <ozancangu24@gmail.com>
2023-02-11 18:36:22 +0300

    -start location updates

--- app/src/main/java/com/ozancanguz/distancetrackerapp/service/TrackerService.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/service/TrackerService.kt
@@ -1,47 +1,68 @@
 package com.ozancanguz.distancetrackerapp.service
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.content.Intent
+import android.location.LocationRequest
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.ozancanguz.distancetrackerapp.utils.Constants.ACTION_SERVICE_START
 import com.ozancanguz.distancetrackerapp.utils.Constants.ACTION_SERVICE_STOP
 import com.ozancanguz.distancetrackerapp.utils.Constants.NOTIFICATION_CHANNEL_ID
 import com.ozancanguz.distancetrackerapp.utils.Constants.NOTIFICATION_CHANNEL_NAME
+import com.ozancanguz.distancetrackerapp.utils.Constants.NOTIFICATION_ID
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
+@AndroidEntryPoint
+class TrackerService : LifecycleService() {
 
-class TrackerService:LifecycleService() {
     @Inject
     lateinit var notification: NotificationCompat.Builder
 
     @Inject
     lateinit var notificationManager: NotificationManager
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
     companion object {
         val started = MutableLiveData<Boolean>()
-    }
+        val startTime = MutableLiveData<Long>()
+        val stopTime = MutableLiveData<Long>()
 
-    override fun onCreate() {
-       setInitialValues()
-        super.onCreate()
+
     }
 
     private fun setInitialValues() {
         started.postValue(false)
+        startTime.postValue(0L)
+        stopTime.postValue(0L)
+
 
     }
+
+    override fun onCreate() {
+        setInitialValues()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        super.onCreate()
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
                 ACTION_SERVICE_START -> {
                     started.postValue(true)
+                    startForegroundService()
+
                 }
                 ACTION_SERVICE_STOP -> {
                     started.postValue(false)
+
                 }
                 else -> {
                 }
@@ -49,6 +70,14 @@ class TrackerService:LifecycleService() {
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+    private fun startForegroundService() {
+        createNotificationChannel()
+        startForeground(NOTIFICATION_ID, notification.build())
+    }
+
+
+
 
 
 
@@ -62,4 +91,7 @@ class TrackerService:LifecycleService() {
             notificationManager.createNotificationChannel(channel)
         }
     }
+
+
 }
+
