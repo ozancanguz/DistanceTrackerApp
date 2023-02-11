064f215ee098d3ad3724b9a1de50b46994e2b4dc
Ozan <ozancangu24@gmail.com>
2023-02-11 15:39:55 +0300

    Revert "-user location permission function implemented"
    This reverts commit 29160d36a74905c3793f196815425eb8acada43b.

--- app/src/main/java/com/ozancanguz/distancetrackerapp/HiltApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.ozancanguz.distancetrackerapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class HiltApplication:Application(){
-}
--- app/src/main/java/com/ozancanguz/distancetrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/ui/MainActivity.kt
@@ -4,26 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 import com.ozancanguz.distancetrackerapp.R
-import com.ozancanguz.distancetrackerapp.utils.Permissions.hasLocationPermission
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.navhostfragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        if (hasLocationPermission(this)) {
-            navController.navigate(R.id.action_permissionFragment_to_mapsFragment)
-        }
 
 
     }
