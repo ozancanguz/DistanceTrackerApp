f44460dea0bedd1025003c96502dae1cc4cbe0bb
Ozan <ozancangu24@gmail.com>
2023-02-11 18:24:14 +0300

    -notification module created

--- app/src/main/java/com/ozancanguz/distancetrackerapp/utils/Constants.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/utils/Constants.kt
@@ -12,13 +12,14 @@ object Constants {
     const val ACTION_SERVICE_START = "ACTION_SERVICE_START"
     const val ACTION_SERVICE_STOP = "ACTION_SERVICE_STOP"
 
-    /*
+
     const val NOTIFICATION_CHANNEL_ID = "tracker_notification_id"
     const val NOTIFICATION_CHANNEL_NAME = "tracker_notification"
     const val NOTIFICATION_ID = 3
 
-    const val PENDING_INTENT_REQUEST_CODE = 99
 
+    const val PENDING_INTENT_REQUEST_CODE = 99
+/*
     const val LOCATION_UPDATE_INTERVAL = 4000L
     const val LOCATION_FASTEST_UPDATE_INTERVAL = 2000L
     */
