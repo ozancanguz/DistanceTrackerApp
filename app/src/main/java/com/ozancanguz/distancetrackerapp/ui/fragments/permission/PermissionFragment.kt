29160d36a74905c3793f196815425eb8acada43b
Ozan <ozancangu24@gmail.com>
2023-02-11 15:39:38 +0300

    -user location permission function implemented

--- app/src/main/java/com/ozancanguz/distancetrackerapp/ui/fragments/permission/PermissionFragment.kt
+++ app/src/main/java/com/ozancanguz/distancetrackerapp/ui/fragments/permission/PermissionFragment.kt
@@ -8,25 +8,54 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.ozancanguz.distancetrackerapp.R
 import com.ozancanguz.distancetrackerapp.databinding.FragmentPermissionBinding
+import com.ozancanguz.distancetrackerapp.utils.Permissions.hasLocationPermission
+import com.ozancanguz.distancetrackerapp.utils.Permissions.requestLocationPermission
+import com.vmadalin.easypermissions.EasyPermissions
+import com.vmadalin.easypermissions.dialogs.SettingsDialog
 
 
-class PermissionFragment : Fragment() {
+class PermissionFragment : Fragment(), EasyPermissions.PermissionCallbacks {
+
     private var _binding: FragmentPermissionBinding? = null
-
     private val binding get() = _binding!!
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-         _binding = FragmentPermissionBinding.inflate(inflater, container, false)
-        val view = binding.root
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentPermissionBinding.inflate(inflater, container, false)
 
+        binding.continueButton.setOnClickListener {
+            if (hasLocationPermission(requireContext())) {
+                findNavController().navigate(R.id.action_permissionFragment_to_mapsFragment)
+            } else {
+                requestLocationPermission(this)
+            }
+        }
 
-        return view
+        return binding.root
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
 
+    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            SettingsDialog.Builder(requireActivity()).build().show()
+        } else {
+            requestLocationPermission(this)
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
+        findNavController().navigate(R.id.action_permissionFragment_to_mapsFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
